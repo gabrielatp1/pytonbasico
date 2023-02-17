89d048ab0a59303cfc51add01076cabe9b726661
gabrielatp1 <125619076+gabrielatp1@users.noreply.github.com>
2023-02-16 21:43:08 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,31 @@
+'''CONDICIONAL'''
+'''idade = 17
+if idade < 18:
+    print("menor de idade")'''
+
+'''idade = 20
+if idade < 18:
+    print ("menor de idade")'''
+
+'''a = int(input("valor1:"))
+b = int(input("valor2:"))
+if a > b:
+    print("o valor 1 é maior!")
+if b > a:
+    print("o valor 2 é maior!")'''
+
+
+'''PASS =  deixar vazio'''
+
+'''ELSE'''
+'''minutos = int(input("Quantos minutos você utilizou este mês? "))
+if minutos < 200:
+    preço = 0.20
+else:
+    if minutos < 400:
+        preço = 0.18
+    else:
+        preço = 0.15
+print(f"Você vai pagar este mês: {minutos*preço} ")'''
+
+'''ELIF = caso não seja IF'''
---
+++ aula7.py
@@ -0,0 +1,21 @@
+'''while'''
+'''x = 1
+while x<=4:
+    print(x)
+    x = x + 1'''
+
+'''x = 1
+while x<=100:
+    print(x)
+    x = x + 1'''
+
+'''x = 50
+while x<=100:
+    print(x)
+    x = x + 1'''
+
+'''x = 10
+while x>=0:
+    print(x)
+    x = x - 1'''
+
